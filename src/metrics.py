732ab7ad7ca10c533e885012d9c51eefbfd6bf1a
Jakov Kordić <jakov.kordic.zg@gmail.com>
2026-01-06 19:19:31 +0100

    Dodane pomoćne funkcije za metrike po smjerovima vehicles_per_direction() i ukupni broj izlazaka total_exits()

--- src/metrics.py
+++ src/metrics.py
@@ -8,3 +8,14 @@ def count_vehicles(occ):
                 if cell:
                     total += 1
     return total
+
+def vehicles_per_direction(occ, d):
+    total = 0
+    for row in occ[d]:
+        for cell in row:
+            if cell:
+                total += 1
+    return total
+
+def total_exits(exits_per_step):
+    return sum(exits_per_step)
