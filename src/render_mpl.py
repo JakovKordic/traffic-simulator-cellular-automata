cb56513c0431f11e14496a13498da9c8692e7539
Jakov Kordić <jakov.kordic.zg@gmail.com>
2026-01-07 08:14:00 +0100

    Dodana funkcija koja stanje occ pretvara u 2d sloj vozila gdje 0.0 znači da nema vozila, a veće vrijednosti {"N": 0.65, "E": 0.75, "S": 0.85, "W": 0.95} služe za diferencijaciju smjera i vizualni prikaz preklapanja

--- src/render_mpl.py
+++ src/render_mpl.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ca import is_intersection
+from ca import DIRS, is_intersection
 
 def build_base_layer(roads):
     h = len(roads)
@@ -16,3 +16,21 @@ def build_base_layer(roads):
                 if is_intersection(roads, y, x):
                     base[y, x] = 0.35
     return base
+
+def occ_to_overlay(occ):
+    h = len(next(iter(occ.values())))
+    w = len(next(iter(occ.values()))[0])
+
+    overlay = np.zeros((h, w), dtype=float)
+
+    dir_value = {"N": 0.65, "E": 0.75, "S": 0.85, "W": 0.95}
+
+    for d in DIRS:
+        lane = occ[d]
+        val = dir_value.get(d, 0.7)
+        for y in range(h):
+            for x in range(w):
+                if lane[y][x]:
+                    overlay[y, x] = max(overlay[y, x], val)
+
+    return overlay
